99f0a22dd66c16523971436314bf1b8120175def
gehuang <ge.huang@htwg-konstanz.de>
2015-11-23 17:07:49 +0100

    rename with de.htwg.se.minesweeper

---
+++ src/de/htwg/se/minesweeper/observer/IMSObserver.java
@@ -0,0 +1,8 @@
+package de.htwg.se.minesweeper.observer;
+
+public interface IMSObserver {
+	/**
+	 * Methode, die aufgerufen wird, wenn der Observable die Observer benachrichtigt.
+	 */
+	void update();
+}
---
+++ src/de/htwg/se/minesweeper/observer/IMSObserverWithArguments.java
@@ -0,0 +1,5 @@
+package de.htwg.se.minesweeper.observer;
+
+public interface IMSObserverWithArguments {
+    void update(Object arg);
+}
