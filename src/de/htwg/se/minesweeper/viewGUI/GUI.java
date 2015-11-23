c9aaca9b3c0bdad6f44369a1111a12f7dca38efa
gehuang <ge.huang@htwg-konstanz.de>
2015-11-23 17:10:03 +0100

    rename with de.htwg.se.minesweeper

---
+++ src/de/htwg/se/minesweeper/viewGUI/GUI.java
@@ -0,0 +1,12 @@
+package de.htwg.se.minesweeper.viewGUI;
+
+import de.htwg.se.minesweeper.controller.MScontroller;
+
+public class GUI {
+	public static void main(String[] args){
+		MScontroller m = new MScontroller();
+		new Gui_Frame(m);
+	}
+	
+
+}
