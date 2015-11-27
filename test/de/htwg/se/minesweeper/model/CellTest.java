8bfaa1780067008c0c0ff730e10f860bfa03d8de
gehuang <ge.huang@htwg-konstanz.de>
2015-11-27 10:29:55 +0100

    add CellTest of Cell

--- test/de/htwg/se/minesweeper/model/CellTest.java
+++ test/de/htwg/se/minesweeper/model/CellTest.java
@@ -7,16 +7,63 @@ import org.junit.Test;
 
 public class CellTest {
 	
-	Cell cell;
-	
+	Cell mb;
+
 	@Before
-	public void setup() {
-		cell = new Cell(0,0);
+	public void setUp() throws Exception {
+		mb = new Cell(12, 34);
 	}
 
 	@Test
-	public void testIsClosed() {
-		assertTrue(cell.isClosed());
+	public void testMineBox() {
+		assertEquals(12, mb.getPosX());
+		assertEquals(34, mb.getPosY());
+	}
+	
+	@Test
+	public void testSetMine() {
+		mb.setMine();
+		assertTrue(mb.isMine());
+	}
+	
+	@Test
+	public void testgetPosX() {
+		assertEquals(12, mb.getPosX());
+	}
+	
+	@Test
+	public void testgetPosY() {
+		assertEquals(34, mb.getPosY());
+	}
+	
+	@Test
+	public void testisMine() {
+		assertFalse(mb.isMine());
+		mb.setMine();
+		assertTrue(mb.isMine());
+	}
+	
+	@Test
+	public void testisCovered() {
+		assertEquals(true, mb.isClosed());
+	}
+	
+	@Test
+	public void testuncover() {
+		mb.open();
+		assertEquals(false, mb.isClosed());
+	}
+
+	@Test
+	public void testsetflag(){
+		mb.setFlag();
+		assertEquals(true, mb.getFlagStatus());
+	}
+	
+	@Test
+	public void testunsetflag(){
+		mb.unsetFlag();
+		assertEquals(false, mb.getFlagStatus());
 	}
 
 }
