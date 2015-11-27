91ae2573a9f2f5f8fa9950f7211de3cd71182097
gehuang <ge.huang@htwg-konstanz.de>
2015-11-27 10:18:26 +0100

    add throw to check failure

--- src/de/htwg/se/minesweeper/minesweeper/Minesweeper.java
+++ src/de/htwg/se/minesweeper/minesweeper/Minesweeper.java
@@ -5,13 +5,13 @@ import de.htwg.se.minesweeper.viewGUI.Gui_Frame;
 import de.htwg.se.minesweeper.viewTUI.TUI;
 
 public class Minesweeper {
-	public static void main(String[] args){
+	public static void main(String[] args) throws InstantiationException{
+		
+		
 		MScontroller m = new MScontroller();
 		new Gui_Frame(m);
 		TUI t = new TUI(m);
-		t.playTUI();
-		
-		
+		t.playTUI(); 
 		
 	}
 
