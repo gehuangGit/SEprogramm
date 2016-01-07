398fed2223a85b699446c8008c75e530d26e4172
gehuang <ge.huang@htwg-konstanz.de>
2016-01-07 11:10:36 +0100

    add JUnit Test to StatusPanel

---
+++ test/de/htwg/se/minesweeper/viewGUI/Gui_StatusPanelTest.java
@@ -0,0 +1,15 @@
+package de.htwg.se.minesweeper.viewGUI;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Gui_StatusPanelTest {
+	Gui_StatusPanel sp = new Gui_StatusPanel();
+	
+	@Test
+	public void testGui_StatusPanel(){
+		assertNotNull(sp);
+	}
+
+}
